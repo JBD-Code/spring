84b8fce02a892f180f0cf4a722a4651a49112074
JBD-Code <lio222@naver.com>
2020-05-13 14:08:49 +0900

    Spring Study 2020-05-12 MultiActionController MVC Update

--- Project03/src/com/spring/ex02/UserController.java
+++ Project03/src/com/spring/ex02/UserController.java
@@ -13,13 +13,15 @@ public class UserController extends MultiActionController {
 		
 		String userID = ""; 
 		String userPWD = "" ;
+		String viewName =getViewName(request);
 		ModelAndView mav = new ModelAndView();
 		request.setCharacterEncoding("UTf-8");
 		userID = request.getParameter("id"); 
 		userPWD = request.getParameter("pwd"); 
 		mav.addObject("userID", userID);
 		mav.addObject("userPWD", userPWD);
-		mav.setViewName("MemberResult");
+		//mav.setViewName("MemberResult");
+		mav.setViewName(viewName);
 		return mav;
 	}
 	
@@ -40,5 +42,36 @@ public class UserController extends MultiActionController {
 		return mav; 
 	}
 	
-	
+	private String getViewName(HttpServletRequest request) throws Exception{
+		String contextPath = request.getContextPath();
+		String uri = (String)request.getAttribute("javax.servlet.include.request_uri");
+		if(uri==null||uri.trim().equals("")) {
+			uri=request.getRequestURI();
+		}
+		
+		int begin = 0; 
+		if(!((contextPath==null)||("".equals(contextPath)))) {
+			begin=contextPath.length();
+		}
+		
+		int end; 
+		
+		if(uri.indexOf(";")!=-1) {
+			end=uri.indexOf(";");
+		}else if(uri.indexOf("?")!=-1) {
+			end=uri.indexOf("?");
+		}else {
+			end=uri.length();
+		}
+		
+		String fileName = uri.substring(begin, end);
+		if(fileName.indexOf(".")!=-1) {
+			fileName=fileName.substring(0,fileName.lastIndexOf("."));
+		}
+		if(fileName.lastIndexOf("/")!=-1) {
+			fileName=fileName.substring(fileName.lastIndexOf("/"),fileName.length());
+			
+		}
+		return fileName;
+	}
 }
