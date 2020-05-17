01253675530d07f1fb9d4a81bb5791d8b5c9b927
JBD-Code <lio222@naver.com>
2020-05-17 14:35:03 +0900

    Spring Study 2020-05-17 Spring & Mybatis Start

---
+++ Project05/src/com/spring/member/dao/MemberDAOImpl.java
@@ -0,0 +1,29 @@
+package com.spring.member.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.dao.DataAccessException;
+
+import com.spring.member.vo.MemberVO;
+
+public class MemberDAOImpl implements MemberDAO{
+	
+	private SqlSession sqlSession ; 
+	
+	
+	public void setSqlSession(SqlSession sqlSession) {
+		this.sqlSession = sqlSession;
+	}
+	
+
+	
+	@Override
+	public List<MemberVO> memberList() throws DataAccessException {
+		List<MemberVO> memberList = null; 
+		memberList = sqlSession.selectList("mapper.member.memberList");
+		return memberList;
+	}
+
+	
+}
