d4067a6b1edaffe8ba9318954e8edf86b8016e7a
JBD-Code <lio222@naver.com>
2020-05-19 13:52:33 +0900

    Spring Study 2020-05-19 Spring 3.0 Annotation @RequestParam

--- Project07/src/com/spring/ex03/MemberController.java
+++ Project07/src/com/spring/ex03/MemberController.java
@@ -1,4 +1,4 @@
-package com.spring.ex02;
+package com.spring.ex03;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -6,30 +6,31 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-/*@Controller("memberController")*/
+@Controller("memberController")
 public class MemberController {
 
-
-
-	@RequestMapping(value= {"/member/memberForm.do","/member/memberForm1.do"}, method = {RequestMethod.GET})
+	@RequestMapping(value = "/member/memberForm.do", method = {RequestMethod.POST, RequestMethod.GET})
 	public ModelAndView memberForm(HttpServletRequest request, HttpServletResponse response) throws Exception{
-		
-		ModelAndView mav = new ModelAndView();
+		request.setCharacterEncoding("UTF-8");
+		ModelAndView mav= new ModelAndView(); 
 		mav.setViewName("memberForm");
 		return mav; 
+		
 	}
-
-	@RequestMapping(value= {"/member/memberResult.do"}, method = {RequestMethod.GET ,RequestMethod.POST})
-	public ModelAndView memberResult(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	//getParameter() X
+	@RequestMapping(value ="/member/memberResult.do", method= {RequestMethod.GET, RequestMethod.POST})
+	public ModelAndView memberResult(@RequestParam("id")String id , 
+									 @RequestParam("pwd")String pwd,
+									 HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
 		request.setCharacterEncoding("UTF-8");
 		ModelAndView mav = new ModelAndView();
-		String id = request.getParameter("id");
-		String pwd = request.getParameter("pwd");
 		mav.setViewName("memberResult");
 		mav.addObject("id", id);
 		mav.addObject("pwd", pwd);
-		return mav; 
+		return mav;  
 	}
 }
