7a6fbb3b531064157a4ce6a3c6460b40bfd90729
JBD-Code <lio222@naver.com>
2020-05-27 13:29:06 +0900

    Spring Study 2020-05-27 Bean Environment

--- Chapter11/src/main/java/com/spring/ex01/AdminConnection.java
+++ Chapter11/src/main/java/com/spring/ex01/AdminConnection.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.core.env.Environment;
-
+/*
 public class AdminConnection implements EnvironmentAware, InitializingBean, DisposableBean  {
 	private Environment env; 
 	private String admin_id; 
@@ -52,6 +52,55 @@ public class AdminConnection implements EnvironmentAware, InitializingBean, Disp
 		System.out.println("==================================");
 		
 	} 
+	
+	
+	
+	
+}
+*/
+public class AdminConnection implements InitializingBean, DisposableBean {
+	private String id;
+	private String pwd; 
+	private String sub_id;
+	private String sub_pwd;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getSub_id() {
+		return sub_id;
+	}
+	public void setSub_id(String sub_id) {
+		this.sub_id = sub_id;
+	}
+	public String getSub_pwd() {
+		return sub_pwd;
+	}
+	public void setSub_pwd(String sub_pwd) {
+		this.sub_pwd = sub_pwd;
+	}
+	
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("Init Bean");
+		
+	} 
+	
+	
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("Destory Bean");
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 	
