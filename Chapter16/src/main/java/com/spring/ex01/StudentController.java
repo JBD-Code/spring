06c1742ac38f06f3696ed134d7df5145548a7fa6
JBD-Code <lio222@naver.com>
2020-05-31 13:30:07 +0900

    Spring Study 2020-05-31 Spring InitBinder

--- Chapter16/src/main/java/com/spring/ex01/StudentController.java
+++ Chapter16/src/main/java/com/spring/ex01/StudentController.java
@@ -1,7 +1,12 @@
 package com.spring.ex01;
 
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,17 +19,22 @@ public class StudentController {
 	 }
 	
 	@RequestMapping("/studentInsert")
-	public String studentCreate(@ModelAttribute("student") Student student, BindingResult result) {
+	public String studentCreate(@ModelAttribute("student") @Valid Student student, BindingResult result) {
 		
 		String page = "result";
-		StudentValidator validator = new StudentValidator();
-		validator.validate(student, result);
+		//StudentValidator validator = new StudentValidator();
+		//validator.validate(student, result);
+		
 		if(result.hasErrors()) {
 			page= "form";
 		}
 		return page;
-	
-	
+		
+		
+	}
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) {
+		binder.setValidator(new StudentValidator());
 	}
 	
 }
