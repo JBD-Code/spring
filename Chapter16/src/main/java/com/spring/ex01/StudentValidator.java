2146f3562e07dbf2230371369a9b0dcfddb7b420
JBD-Code <lio222@naver.com>
2020-05-31 13:09:27 +0900

    Spring Study 2020-05-31 Spring ValidationUtils

--- Chapter16/src/main/java/com/spring/ex01/StudentValidator.java
+++ Chapter16/src/main/java/com/spring/ex01/StudentValidator.java
@@ -1,6 +1,7 @@
 package com.spring.ex01;
 
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 public class StudentValidator implements Validator {
@@ -16,7 +17,8 @@ public class StudentValidator implements Validator {
 		// TODO Auto-generated method stub
 		System.out.println("validate");
 		Student student = (Student)target;	
-	
+		
+		/*
 		String studentName= student.getName();
 		if(studentName ==null||studentName.trim().isEmpty()) {
 			System.out.println("studentNameName is null or empty" );
@@ -27,11 +29,19 @@ public class StudentValidator implements Validator {
 			System.out.println("StudentAge is Null or empty ");
 			errors.reject("age", "trouble");
 		}
+		
+		
 		String studentEmail = student.getEmail();
 		if(studentEmail ==null || studentEmail.trim().isEmpty()) {
 			System.out.println("studentNameName is null or empty" );
 			errors.reject("email", "trouble");
 		}
+		 */
+		ValidationUtils.rejectIfEmpty(errors, "name", "troble");
+		ValidationUtils.rejectIfEmpty(errors, "age", "troble");
+		ValidationUtils.rejectIfEmpty(errors, "email", "troble");
+		
+	
 	}
 	
 }
