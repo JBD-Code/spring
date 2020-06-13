04f389156651d766450a36730f35f1c575e999a6
JBD-Code <lio222@naver.com>
2020-06-13 16:20:41 +0900

    Spring Study 2020-06-13 JDBCTemplate

--- Chapter17/src/main/java/com/spring/ex01/dao/BoardDAO.java
+++ Chapter17/src/main/java/com/spring/ex01/dao/BoardDAO.java
@@ -14,6 +14,8 @@ import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.PreparedStatementSetter;
 
 import com.spring.ex01.dto.BoardDTO;
 import com.spring.ex01.util.Constant;
@@ -100,18 +102,19 @@ public class BoardDAO {
 	*/
 	
 	public List<BoardDTO>list() {
-		List<BoardDTO>lists = new ArrayList<BoardDTO>();
+		//List<BoardDTO>lists = new ArrayList<BoardDTO>();
 		
 		sql ="select level, idx, bgroup, title, content, id, name, writeDate, bStep, bIndent, readCount"
 				+ " from board "
 				+ " start with bgroup=0"
 				+ " connect by prior idx=bgroup"
 				+ " order siblings by idx desc";
-		lists  = template.query(sql, new BeanPropertyRowMapper<BoardDTO>(BoardDTO.class));
+		return template.query(sql, new BeanPropertyRowMapper<BoardDTO>(BoardDTO.class));
 	
-		return lists;
+		//return lists;
 	}	
 	
+	/*
 	public void insert(int idx, String id, String name, String title, String content, Date writeDate) {
 		
 		idx = idxCount(idx);
@@ -140,7 +143,32 @@ public class BoardDAO {
 		
 		
 	}
+	*/
+	public void insert(int idx, final String id, final String name, final String title, final String content, final Date writeDate) {
+			
+		final int index = idxCount(idx);
+		template.update(new PreparedStatementCreator() {
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				sql = "insert into board(idx, id, name, title, content, writeDate, bGroup, bStep, bIndent)"
+						+ " values(?,?,?,?,?,?,?,?,?)";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setInt(1, index);
+				pstmt.setString(2, id);
+				pstmt.setString(3, name);
+				pstmt.setString(4, title);
+				pstmt.setString(5, content);
+				pstmt.setDate(6, writeDate);
+				pstmt.setInt(7, 0);
+				pstmt.setInt(8, 0);
+				pstmt.setInt(9, 0);
+				
+				return pstmt;
+			}
+		});
+	}
 	
+	/*
 	public int idxCount(int idx) {
 		
 		try {
@@ -159,7 +187,15 @@ public class BoardDAO {
 		} 
 		return idx+1;
 	}
-
+	*/
+	public int idxCount(int idx) {
+		sql= "select count(*) from board";
+		
+		 return template.queryForInt(sql)+1;
+		
+	}
+	
+	/*
 	public BoardDTO select(int idx) {
 		BoardDTO dto = new BoardDTO(); 
 		readCount(idx);
@@ -190,7 +226,14 @@ public class BoardDAO {
 		// TODO Auto-generated method stub
 		return dto; 
 	}
-
+	*/
+	public BoardDTO select(int idx) {
+		readCount(idx);
+		sql= "select * from board where idx="+idx; 
+		return template.queryForObject(sql, new BeanPropertyRowMapper<BoardDTO>(BoardDTO.class));
+		
+	}
+	/*
 	public void modify(int idx, String name, String title, String content) {
 		
 		try {
@@ -210,7 +253,25 @@ public class BoardDAO {
 		}
 		
 	}
+	*/
+	public void modify(final int idx, final String name, final String title, final String content) {
+		
+		sql= "update board set name=?, title=?, content=? where idx=?"; 
+		template.update(sql, new PreparedStatementSetter() {
+			
+			@Override
+			public void setValues(PreparedStatement ps) throws SQLException {
+				// TODO Auto-generated method stub
+				ps.setString(1, name);
+				ps.setString(2, title);
+				ps.setString(3, content);
+				ps.setInt(4, idx);
 	
+			}
+		});
+	}
+	
+	/*
 	public void readCount(int idx) {
 		
 		try {
@@ -228,6 +289,21 @@ public class BoardDAO {
 
 	}
 
+	*/
+	public void readCount(final int idx) {
+		sql = "update board set readCount=readCount+1 where idx=?";
+		
+		template.update(sql, new PreparedStatementSetter() {
+			
+			@Override
+			public void setValues(PreparedStatement ps) throws SQLException {
+				// TODO Auto-generated method stub
+				ps.setInt(1, idx);
+			}
+		});
+	}	
+	
+	/*
 	public void delete(int idx) {
 		try {
 			conn=getConnection();
@@ -243,7 +319,19 @@ public class BoardDAO {
 		}
 		
 	}
+	*/
+	public void delete(final int idx) {
+		sql = "delete from board where idx = ?"; 
+		template.update(sql, new PreparedStatementSetter() {
 
+			@Override
+			public void setValues(PreparedStatement ps) throws SQLException {
+				ps.setInt(1, idx);
+			}
+		});
+	}
+
+	/*
 	public void insertReply(int idx, String id, String name, String title, String content, int bgroup,
 			Date writeDate) {
 		idx=idxCount(idx);
@@ -267,7 +355,33 @@ public class BoardDAO {
 		}
 		
 	}
+	*/
+	
+	public void insertReply(int idx, final String id, final String name, final String title, final String content, final int bgroup,
+			final Date writeDate) {
+		final int index=idxCount(idx);
+		
+		sql="insert into board(idx, id, name, title, content, bgroup, writeDate )"
+				+ " values(?,?,?,?,?,?,?)";
+		template.update(new PreparedStatementCreator() {
+
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				pstmt = con.prepareStatement(sql);
+				pstmt.setInt(1, index);
+				pstmt.setString(2, id);
+				pstmt.setString(3, name);
+				pstmt.setString(4, title);
+				pstmt.setString(5, content);
+				pstmt.setInt(6, bgroup);
+				pstmt.setDate(7, writeDate);
+				
+				return pstmt;
+			}
+			
+		});
 	
 	
+	}	
 	
 }
