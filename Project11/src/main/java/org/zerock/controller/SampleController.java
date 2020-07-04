65aa37452b790023b917e3588e807ba10a5b61bc
JBD-Code <lio222@naver.com>
2020-07-04 13:54:36 +0900

    Spring Study 2020-07-04 JSON ResponseEntity

--- Project11/src/main/java/org/zerock/controller/SampleController.java
+++ Project11/src/main/java/org/zerock/controller/SampleController.java
@@ -5,13 +5,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.zerock.domain.SampleDTO;
 import org.zerock.domain.SampleDTOList;
 import org.zerock.domain.TodoDTO;
@@ -99,6 +104,57 @@ public class SampleController {
 	//Parameter 은 [index]와 같은 형태로 전달이 가능하다 
 	//sample/ex05Bean?list[0].name=add&list[2].name=bbb	형태로 호출 가능 
 	}
+	
+	@GetMapping("/ex07")
+	//public String ex07(SampleDTO dto, int page) {
+	public String ex07(SampleDTO dto, @ModelAttribute("page") int page) {
+		log.info("ex07");
+		log.info("dto : " + dto);
+		log.info("page : " + page);
+		
+		return "/sample/ex07";
+	//localhost:8090/sample/ex07?name=aaa&age=11&page=9 라고 호출할 경우 
+	//views/sample/ex07.jsp 의 View 에는 SampleDTO만이 전달 된 것을 확인 가능하다 
+	//int type 로 선언된 page는 전달이 되지 않는다 
+	//이런 경우 @ModelAttribute 를 이용헤 Model 에 담아서 전달한다 	
+	//localhost:8090/sample/ex07?name=aaa&age=11&page=9 라고 호출할 경우 
+	//views/sample/ex07.jsp SampleDTO와 page 가 화면에 표시되는 것을 확인 가능 하다 		
+	
+	}
+	
+	
+	@GetMapping("/ex08")
+	public @ResponseBody SampleDTO ex08 () {
+		log.info("/ex08");
+		SampleDTO dto = new SampleDTO();
+		dto.setAge(10);
+		dto.setName("홍길동");
+		return dto; 
+
+	//Controller의 메서드에서 return 타입읍 VO DTO 타입 등 복합적인 데이터가 들어간 객체 타입으로 지정이 가능 
+	//이 경우에는 주로 JSON 데이터를 만들어 내는 경우에 사용한다 
+	//이를 위해서는 pom.xml에서 jackson-databind 라이브러리를 추가한다 
+	//@ResponBody Annotation을 이용 
+	}
+	
+	@GetMapping("/ex09")
+	public ResponseEntity<String> ex09() {
+		log.info("ex09");
+		String msg = "{\"name\":\"홍길동\"}";
+		log.info("msg  : " + msg);
+		HttpHeaders header = new HttpHeaders();
+		header.add("Content-Type", "application/json;charset=UTF-8");
+		
+		return new ResponseEntity<>(msg, header, HttpStatus.OK);
+	//WEB를 다루다 보면 HTTP 프로토콜의 헤더를 다루는 경우가 종종 있다 
+	//Spring MVC 사상은 HttpServletRequest, HttpServletResponse 를 직접 핸들링 하지 않아도 
+	//이런 작업이 가능 하도록 작성되었기 때문에 이러한 처리를 위해서 ResponseEntity를 통해서
+	//원하는 헤더 정보, 데이터 전달이 가능하다 
+	
+	
+	}
+	
+	
 	
 	
 }
--- Project11/src/main/java/org/zerock/domain/SampleDTO.java
+++ Project11/src/main/java/org/zerock/domain/SampleDTO.java
@@ -7,6 +7,6 @@ public class SampleDTO {
 	//Lombok의  @Data Annotation 을 이용하면 getter/setter , equals(), toString의 메서드를 자동 생성  
 	
 	private String name;
-	private String age;
+	private int age;
 	
 }
